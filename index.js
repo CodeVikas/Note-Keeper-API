ff3f9f27da05f9563c7b86483f509f76f723dd6a
Vikas <vikasnagpure03@gmail.com>
2025-07-31 22:48:15 +0530

    changes

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const authRoutes = require('./Routes/authRoute');
-const noteRoutes = require('./Routes/noteRoutes');
+const authRoutes = require('../routes/authRoute');
+const noteRoutes = require('../routes/noteRoutes');
 
 const app = express();
 
-// ✅ Middleware to parse JSON
 app.use(express.json());
-
-// ✅ Route middlewares
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
-// ✅ Connect to MongoDB and start server
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(5000, () => console.log('Server running on port 5000'));
-  })
-  .catch((err) => console.log(err));
+// Mongo connection (make sure it's only called once)
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('MongoDB connected');
+});
+
+module.exports.handler = serverless(app);
